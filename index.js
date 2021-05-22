6a9698ea979d030f82ecd750a3b742e633bdf3c6
Pawan Kumar Regoti <regotipawan@gmail.com>
2021-05-22 08:30:03 +0200

    added package cors-anywhere

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3030
 
 corsProxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
+    originWhitelist: ['','pawanregoti.github.io/'], // Allow all origins
+    requireHeader: [],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
