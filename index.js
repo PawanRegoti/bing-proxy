dd5c4a2915ab1d2d13364dfda86ac4b751cca0cf
Pawan Kumar Regoti <regotipawan@gmail.com>
2021-05-22 08:51:26 +0200

    removed empty origin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3030
 
 corsProxy.createServer({
-    originWhitelist: ['','https://pawanregoti.github.io'], // Allow all origins
+    originWhitelist: ['https://pawanregoti.github.io'], // Allow all origins
     requireHeader: ['Origin', 'X-Requested-With'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
